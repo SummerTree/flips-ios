3581d639bd5a2a82b13747b4398750b2713946d1
arctouch-ecilteodoro <ecil.teodoro@arctouch.com>
2014-09-25 11:41:36 -0300

    Updated flowerbox

--- mugchat/mugchat/UserService.swift
+++ mugchat/mugchat/UserService.swift
@@ -1,10 +1,15 @@
 //
-//  UserService.swift
-//  mugchat
+// Copyright 2014 ArcTouch, Inc.
+// All rights reserved.
 //
-//  Created by Ecil Teodoro on 9/23/14.
-//  Copyright (c) 2014 ArcTouch Inc. All rights reserved.
+// This file, its contents, concepts, methods, behavior, and operation
+// (collectively the "Software") are protected by trade secret, patent,
+// and copyright laws. The use of the Software is governed by a license
+// agreement. Disclosure of the Software to third parties, in any form,
+// in whole or in part, is expressly prohibited except as authorized by
+// the license agreement.
 //
+
 public typealias ServiceResponse = (MugError?, User?) -> Void
 
 public class UserService: MugchatService {
