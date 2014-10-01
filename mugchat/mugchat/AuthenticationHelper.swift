c9e40088900645c39fbb8d6cfa9e71362144bc4b
Diego Santiviago <diego.santiviago@arctouch.com>
2014-10-01 14:19:26 -0300

    Creating a Singleton to manage the authenticated user

---
+++ mugchat/mugchat/AuthenticationHelper.swift
@@ -0,0 +1,39 @@
+//
+// Copyright 2014 ArcTouch, Inc.
+// All rights reserved.
+//
+// This file, its contents, concepts, methods, behavior, and operation
+// (collectively the "Software") are protected by trade secret, patent,
+// and copyright laws. The use of the Software is governed by a license
+// agreement. Disclosure of the Software to third parties, in any form,
+// in whole or in part, is expressly prohibited except as authorized by
+// the license agreement.
+//
+
+import Foundation
+
+public class AuthenticationHelper: NSObject {
+    
+    private let LOGIN_USERNAME_KEY = "username"
+    
+    func saveAuthenticatedUsername(username: String) {
+        var userDefaults = NSUserDefaults.standardUserDefaults()
+        userDefaults.setValue(username, forKey: LOGIN_USERNAME_KEY)
+        userDefaults.synchronize()
+    }
+    
+    func retrieveAuthenticatedUsernameIfExists() -> String? {
+        var userDefaults = NSUserDefaults.standardUserDefaults()
+        return userDefaults.valueForKey(LOGIN_USERNAME_KEY) as String?
+    }
+    
+    
+    // MARK: - Singleton method
+    
+    public class var sharedInstance : AuthenticationHelper {
+    struct Static {
+        static let instance : AuthenticationHelper = AuthenticationHelper()
+        }
+        return Static.instance
+    }
+}
