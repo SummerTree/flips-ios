5b5a44b5e131dd47ffae173f252304ea3f5b9819
arctouch-fernandoghisi <fernando.ghisi@arctouch.com>
2014-10-01 12:01:49 -0300

    Forgot Password: Improving view

--- mugchat/mugchat/ForgotPasswordView.swift
+++ mugchat/mugchat/ForgotPasswordView.swift
@@ -12,7 +12,7 @@
 
 import Foundation
 
-class ForgotPasswordView : UIView {
+class ForgotPasswordView : UIView, UITextFieldDelegate {
     
     var delegate: ForgotPasswordViewDelegate?
     
@@ -21,9 +21,32 @@ class ForgotPasswordView : UIView {
     private let MARGIN_BOTTOM:CGFloat = 10.0
     private let MARGIN_LEFT:CGFloat = 40.0
     
+    
+    private let TOP_MARGIN: CGFloat = 44.0
+    private let HINT_VIEW_MARGIN_LEFT: CGFloat = 25.0
+    private let HINT_VIEW_MARGIN_RIGHT: CGFloat = 25.0
+    private let MOBILE_NUMBER_MARGIN_LEFT: CGFloat = 25.0
+    private let MOBILE_NUMBER_MARGIN_RIGHT: CGFloat = 25.0
+    private let MOBILE_NUMBER_VIEW_HEIGHT: CGFloat = 60.0
+    private let MOBILE_TEXT_FIELD_LEADING: CGFloat = 58.0
+    
+    private let HINT_TEXT: String = "Enter your phone number below\n to reset your password"
+    private let SPAM_TEXT: String = "That whole spam thing...\nYeah, we don't do that."
+    
     private var navigationBar: CustomNavigationBar!
-    private var enterMobileNumberLabel: UILabel!
-    private var mobileNumberTextField: UITextField!
+    //private var enterMobileNumberLabel: UILabel!
+    //private var mobileNumberTextField: UITextField!
+    
+    var hintView: UIView!
+    var hintText: UILabel!
+    var mobileNumberView: UIView!
+    var phoneImageView: UIImageView!
+    var mobileNumberField: UITextField!
+    var spamView: UIView!
+    var spamText: UILabel!
+    var keyboardFillerView: UIView!
+    
+    var keyboardHeight: CGFloat = 0.0
     
     
     override init() {
@@ -33,17 +56,17 @@ class ForgotPasswordView : UIView {
         self.makeConstraints()
     }
     
-    func viewDidLoad() {
-        
-    }
-    
-    func viewDidAppear() {
-        
-    }
-    
-    func viewWillDisappear() {
-        
-    }
+//    func viewDidLoad() {
+//        
+//    }
+//    
+//    func viewDidAppear() {
+//        
+//    }
+//    
+//    func viewWillDisappear() {
+//        
+//    }
     
     func addSubviews() {
         
@@ -51,7 +74,7 @@ class ForgotPasswordView : UIView {
         //navigationBar.delegate = self
         self.addSubview(navigationBar)
         
-        enterMobileNumberLabel = UILabel()
+        /*enterMobileNumberLabel = UILabel()
         enterMobileNumberLabel.font = UIFont.avenirNextRegular(UIFont.HeadingSize.h4)
         enterMobileNumberLabel.text = "Enter your phone number below to reset your password"
         enterMobileNumberLabel.textAlignment = NSTextAlignment.Center;
@@ -59,7 +82,54 @@ class ForgotPasswordView : UIView {
         enterMobileNumberLabel.numberOfLines = 0; //unlimited lines
         //enterMobileNumberLabel.sizeToFit()
         enterMobileNumberLabel.textColor = UIColor.whiteColor()
-        self.addSubview(enterMobileNumberLabel)
+        self.addSubview(enterMobileNumberLabel)*/
+        
+        hintView = UIView()
+        hintView.contentMode = .Center
+        self.addSubview(hintView)
+        
+        hintText = UILabel()
+        hintText.numberOfLines = 0
+        hintText.textAlignment = NSTextAlignment.Center
+        hintText.text = NSLocalizedString(HINT_TEXT, comment: HINT_TEXT)
+        hintText.textColor = UIColor.whiteColor()
+        hintText.font = UIFont.avenirNextRegular(UIFont.HeadingSize.h4)
+        hintView.addSubview(hintText)
+        
+        mobileNumberView = UIView()
+        mobileNumberView.contentMode = .Center
+        mobileNumberView.backgroundColor = UIColor.whiteColor().colorWithAlphaComponent(0.25)
+        self.addSubview(mobileNumberView)
+        
+        phoneImageView = UIImageView(image: UIImage(named: "Phone"))
+        phoneImageView.contentMode = .Center
+        mobileNumberView.addSubview(phoneImageView)
+        
+        mobileNumberField = UITextField()
+        mobileNumberField.delegate = self
+        mobileNumberField.becomeFirstResponder()
+        mobileNumberField.textColor = UIColor.whiteColor()
+        mobileNumberField.tintColor = UIColor.whiteColor()
+        mobileNumberField.font = UIFont.avenirNextMedium(UIFont.HeadingSize.h4)
+        mobileNumberField.attributedPlaceholder = NSAttributedString(string: NSLocalizedString("Mobile Number", comment: "Mobile Number"), attributes: [NSForegroundColorAttributeName: UIColor.whiteColor(), NSFontAttributeName: UIFont.avenirNextUltraLight(UIFont.HeadingSize.h4)])
+        mobileNumberField.keyboardType = UIKeyboardType.PhonePad
+        mobileNumberView.addSubview(mobileNumberField)
+        
+        spamView = UIView()
+        spamView.contentMode = .Center
+        self.addSubview(spamView)
+        
+        spamText = UILabel()
+        spamText.numberOfLines = 0
+        spamText.textAlignment = NSTextAlignment.Center
+        spamText.text = NSLocalizedString(SPAM_TEXT, comment: SPAM_TEXT)
+        spamText.textColor = UIColor.whiteColor()
+        spamText.font = UIFont.avenirNextRegular(UIFont.HeadingSize.h6)
+        spamView.addSubview(spamText)
+        
+        keyboardFillerView = UIView()
+        keyboardFillerView.backgroundColor = UIColor.greenColor()
+        self.addSubview(keyboardFillerView)
         
     }
     
@@ -72,14 +142,90 @@ class ForgotPasswordView : UIView {
             make.height.equalTo()(self.navigationBar.frame.size.height)
         }
         
-        enterMobileNumberLabel.mas_makeConstraints { (make) -> Void in
-            //make.centerX.equalTo()(self)
+        /*enterMobileNumberLabel.mas_makeConstraints { (make) -> Void in
             make.left.offset()(self.MARGIN_LEFT);
             make.right.offset()(-self.MARGIN_RIGHT);
             make.top.equalTo()(self.navigationBar.mas_bottom).with().offset()(self.MARGIN_TOP)
+        }*/
+        
+        hintView.mas_updateConstraints { (make) in
+            make.top.equalTo()(self).with().offset()(self.TOP_MARGIN)
+            make.left.equalTo()(self).with().offset()(self.HINT_VIEW_MARGIN_LEFT)
+            make.right.equalTo()(self).with().offset()(-self.HINT_VIEW_MARGIN_RIGHT)
         }
         
+        hintText.mas_updateConstraints { (make) in
+            make.centerY.equalTo()(self.hintView)
+            make.centerX.equalTo()(self.hintView)
+        }
+        
+        mobileNumberView.mas_updateConstraints { (make) in
+            make.top.equalTo()(self.hintView.mas_bottom)
+            make.height.equalTo()(self.MOBILE_NUMBER_VIEW_HEIGHT)
+            make.left.equalTo()(self)
+            make.right.equalTo()(self)
+        }
+        
+        phoneImageView.mas_updateConstraints { (make) in
+            make.left.equalTo()(self.mobileNumberView).with().offset()(self.MOBILE_NUMBER_MARGIN_LEFT)
+            make.centerY.equalTo()(self.mobileNumberView)
+            make.width.equalTo()(self.phoneImageView.image?.size.width)
+        }
+        
+        mobileNumberField.mas_updateConstraints { (make) in
+            make.left.equalTo()(self).with().offset()(self.MOBILE_TEXT_FIELD_LEADING)
+            make.centerY.equalTo()(self.mobileNumberView)
+        }
+        
+        spamView.mas_updateConstraints({ (make) in
+            make.top.equalTo()(self.mobileNumberView.mas_bottom)
+            make.left.equalTo()(self).with().offset()(self.HINT_VIEW_MARGIN_LEFT)
+            make.right.equalTo()(self).with().offset()(-self.HINT_VIEW_MARGIN_RIGHT)
+            make.height.equalTo()(self.hintView)
+        })
+        
+        spamText.mas_updateConstraints { (make) in
+            make.centerY.equalTo()(self.spamView)
+            make.centerX.equalTo()(self.spamView)
+        }
+        
+        keyboardFillerView.mas_updateConstraints( { (make) in
+            make.top.equalTo()(self.spamView.mas_bottom)
+            make.left.equalTo()(self)
+            make.right.equalTo()(self)
+            make.height.equalTo()(self.keyboardHeight)
+            make.bottom.equalTo()(self)
+        })
+        
         super.updateConstraints()
+    }
+    
+    func textField(textField: UITextField, shouldChangeCharactersInRange range: NSRange, replacementString string: String) -> Bool {
+        
+        let text = textField.text
+        let length = countElements(text)
+        var shouldReplace = true
+        
+        if (string != "") {
+            switch length {
+            case 3, 7:
+                textField.text = "\(text)-"
+            default:
+                break;
+            }
+            if (length > 11) {
+                shouldReplace = false
+            }
+        } else {
+            switch length {
+            case 5, 9:
+                let nsString = text as NSString
+                textField.text = nsString.substringWithRange(NSRange(location: 0, length: length-1)) as String
+            default:
+                break;
+            }
+        }
+        return shouldReplace;
     }
     
     
--- mugchat/mugchat/ForgotPasswordViewController.swift
+++ mugchat/mugchat/ForgotPasswordViewController.swift
@@ -12,9 +12,25 @@
 
 import UIKit
 
-class ForgotPasswordViewController: UIViewController, ForgotPasswordViewDelegate {
+class ForgotPasswordViewController: MugChatViewController, ForgotPasswordViewDelegate {
     
     var forgotPasswordView: ForgotPasswordView!
+    
+    /*override init() {
+        super.init()
+        let center = NSNotificationCenter.defaultCenter()
+        center.addObserver(self, selector: "keyboardOnScreen:", name: UIKeyboardDidShowNotification, object: nil)
+    }*/
+    
+    /*override init(nibName nibNameOrNil: String!, bundle nibBundleOrNil: NSBundle!) {
+        super.init(nibName: nil, bundle: nil)
+        let center = NSNotificationCenter.defaultCenter()
+        center.addObserver(self, selector: "keyboardOnScreen:", name: UIKeyboardDidShowNotification, object: nil)
+    }
+    
+    required init(coder: NSCoder) {
+        fatalError("NSCoding not supported")
+    }*/
     
     override func viewDidLoad() {
         super.viewDidLoad()
@@ -22,7 +38,6 @@ class ForgotPasswordViewController: UIViewController, ForgotPasswordViewDelegate
         forgotPasswordView = ForgotPasswordView()
         forgotPasswordView.delegate = self
         self.view = forgotPasswordView
-        forgotPasswordView.viewDidLoad()
     }
     
     override func viewWillAppear(animated: Bool) {
@@ -32,19 +47,34 @@ class ForgotPasswordViewController: UIViewController, ForgotPasswordViewDelegate
     
     override func viewDidAppear(animated: Bool) {
         super.viewDidAppear(animated)
-        forgotPasswordView.viewDidAppear()
     }
     
     override func viewWillDisappear(animated: Bool) {
         self.navigationController?.navigationBarHidden = false
         super.viewWillDisappear(animated)
-        forgotPasswordView.viewWillDisappear()
     }
     
     
     func forgotPasswordViewDidFinishTypingMobileNumber(forgotPassword: ForgotPasswordView!) {
+        //TODO: open VerificationCode screen (story 7153)
         //var verificationCodeViewController = VerificationCodeViewController()
         //self.navigationController?.pushViewController(verificationCodeViewController, animated: true)
+    }
+    
+    
+    // MARK: - Notifications
+    func keyboardOnScreen(notification: NSNotification) {
+        if let info = notification.userInfo {
+            let keyboardFrame: CGRect = (info[UIKeyboardFrameEndUserInfoKey] as NSValue).CGRectValue()
+            forgotPasswordView.keyboardHeight = keyboardFrame.height
+            forgotPasswordView.updateConstraints()
+        }
+    }
+    
+    
+    // MARK: - CustomNavigationBarDelegate Methods
+    func customNavigationBarDidTapLeftButton(navBar : CustomNavigationBar) {
+        self.navigationController?.popViewControllerAnimated(true)
     }
 
 }
