7838883222ffc7e4caa49fafb348f6b2559e637f
arctouch-ecilteodoro <ecil.teodoro@arctouch.com>
2014-10-03 10:46:59 -0300

    Reverted local changes on AppDelegate

--- mugchat/mugchat/AppDelegate.swift
+++ mugchat/mugchat/AppDelegate.swift
@@ -22,12 +22,9 @@ class AppDelegate: UIResponder, UIApplicationDelegate {
         
         self.window = UIWindow(frame: UIScreen.mainScreen().bounds)
         
-//        let loginViewController = LoginViewController()
-//        let navigationViewControler = UINavigationController(rootViewController: loginViewController)
+        let loginViewController = LoginViewController()
+        let navigationViewControler = UINavigationController(rootViewController: loginViewController)
        
-        let vc = PhoneNumberViewController()
-        let navigationViewControler = UINavigationController(rootViewController: vc)
-        
         self.window?.rootViewController = navigationViewControler
         self.window?.makeKeyAndVisible()
         
