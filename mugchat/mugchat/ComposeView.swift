462c9ddf88dcdf46530a7b9aee7c72930b042ee1
Bruno Guerios <brunoguerios@gmail.com>
2014-11-05 13:39:33 -0200

    Adds a gradient to both camera preview and mug image views on compose view.

--- mugchat/mugchat/ComposeView.swift
+++ mugchat/mugchat/ComposeView.swift
@@ -32,6 +32,7 @@ class ComposeView : UIView, CustomNavigationBarDelegate, CameraViewDelegate, Mug
     
     private var mugContainerView: UIView!
     private var mugImageView: UIImageView!
+    private var mugFilterImageView: UIImageView!
     private var mugWordLabel: UILabel!
     private var centeredMugsView: MugsTextsView!
     private var mugTextsContainerSeparator : UIView!
@@ -128,6 +129,11 @@ class ComposeView : UIView, CustomNavigationBarDelegate, CameraViewDelegate, Mug
         mugImageView.alpha = 0.0
         mugImageView.contentMode = UIViewContentMode.ScaleAspectFit
         mugContainerView.addSubview(mugImageView)
+        
+        mugFilterImageView = UIImageView(image: UIImage(named: "Filter_Photo"))
+        mugFilterImageView.alpha = 1.0
+        mugFilterImageView.contentMode = UIViewContentMode.ScaleAspectFit
+        mugContainerView.addSubview(mugFilterImageView)
         
         mugWordLabel = UILabel()
         mugWordLabel.font = UIFont.avenirNextBold(UIFont.HeadingSize.h1)
@@ -251,6 +257,21 @@ class ComposeView : UIView, CustomNavigationBarDelegate, CameraViewDelegate, Mug
             }
             
             make.height.equalTo()(self.mugImageView.mas_width)
+        }
+        
+        mugFilterImageView.mas_makeConstraints { (make) -> Void in
+            make.top.equalTo()(self.mugContainerView)
+            make.bottom.equalTo()(self.mugImageView)
+            
+            if (DeviceHelper.sharedInstance.isDeviceModelLessOrEqualThaniPhone4S()) {
+                make.centerX.equalTo()(self.mugContainerView)
+                make.width.equalTo()(self.MUG_IMAGE_WIDTH)
+            } else {
+                make.left.equalTo()(self.mugContainerView)
+                make.right.equalTo()(self.mugContainerView)
+            }
+            
+            make.height.equalTo()(self.mugFilterImageView.mas_width)
         }
         
         mugWordLabel.mas_makeConstraints { (make) -> Void in
@@ -590,6 +611,10 @@ class ComposeView : UIView, CustomNavigationBarDelegate, CameraViewDelegate, Mug
     
     func getMugImageView() -> UIImageView {
         return self.mugImageView
+    }
+    
+    func getMugFilterImageView() -> UIImageView {
+        return self.mugFilterImageView
     }
     
     func getMugWord() -> String {
