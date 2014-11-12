8fd7ceeed3347b33786fe2b55926c1877de9858d
Diego Santiviago <diego.santiviago@arctouch.com>
2014-11-12 15:57:17 -0200

    Fixing header file

--- mugchat/mugchat/ContactListHelper.swift
+++ mugchat/mugchat/ContactListHelper.swift
@@ -1,9 +1,16 @@
 //
-//  ContactListHelper.swift
+//  NewFlipViewController.swift
 //  mugchat
 //
-//  Created by Diego Santiviago on 11/12/14.
+// Copyright 2014 ArcTouch, Inc.
+// All rights reserved.
 //
+// This file, its contents, concepts, methods, behavior, and operation
+// (collectively the "Software") are protected by trade secret, patent,
+// and copyright laws. The use of the Software is governed by a license
+// agreement. Disclosure of the Software to third parties, in any form,
+// in whole or in part, is expressly prohibited except as authorized by
+// the license agreement.
 //
 
 public typealias ContactListSuccessResponse = (Array<ContactListHelper.Contact>?) -> Void
