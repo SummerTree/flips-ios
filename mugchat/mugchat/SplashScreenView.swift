9fcd0dcde514ac4a49ffe738acbd973fe41af9a9
Bruno Guerios <brunoguerios@gmail.com>
2014-11-27 12:56:22 -0200

    Implements new splash screen.

--- mugchat/mugchat/SplashScreenView.swift
+++ mugchat/mugchat/SplashScreenView.swift
@@ -14,30 +14,15 @@ import UIKit
 
 class SplashScreenView: UIView {
     
-    var backgroundImage: UIImageView!
     var delegate: SplashScreenViewDelegate?
     
-    override init() {
-        super.init()
-        self.addSubviews()
-        self.makeConstraints()
-    }
+    private let MARGIN_RIGHT:CGFloat = 40.0
+    private let MARGIN_LEFT:CGFloat = 40.0
+    private let MUGCHAT_WORD_LOGO_MARGIN_TOP: CGFloat = 15.0
     
-    func addSubviews() {
-        self.backgroundImage = UIImageView(image: UIImage(named: "SplashScreen"))
-        self.backgroundImage.contentMode = UIViewContentMode.ScaleAspectFill
-        self.addSubview(self.backgroundImage)
-    }
-    
-    func makeConstraints() {
-        backgroundImage.mas_makeConstraints { (make) -> Void in
-            make.centerX.equalTo()(self)
-            make.centerY.equalTo()(self)
-            make.height.equalTo()(self)
-            make.width.equalTo()(self)
-        }
-    }
-    
+    private var logoView: UIView!
+    private var bubbleChatImageView: UIImageView!
+    private var mugchatWordImageView: UIImageView!
     
     // MARK: - Required inits
     
@@ -48,7 +33,63 @@ class SplashScreenView: UIView {
     required init(coder aDecoder: NSCoder) {
         fatalError("init(coder:) has not been implemented")
     }
+    
+    override init() {
+        super.init()
+        self.backgroundColor = UIColor.redColor()
+        self.addSubviews()
+        self.makeConstraints()
+    }
+    
+    func addSubviews() {
+        
+        logoView = UIView()
+        self.addSubview(logoView)
+        
+        bubbleChatImageView = UIImageView(image: UIImage(named: "ChatBubble"))
+        bubbleChatImageView.sizeToFit()
+        bubbleChatImageView.contentMode = UIViewContentMode.Center
+        logoView.addSubview(bubbleChatImageView)
+        
+        mugchatWordImageView = UIImageView(image: UIImage(named: "MugChatWord"))
+        mugchatWordImageView.sizeToFit()
+        mugchatWordImageView.contentMode = UIViewContentMode.Center
+        logoView.addSubview(mugchatWordImageView)
+    }
+    
+    func makeConstraints() {
+        
+        logoView.mas_makeConstraints { (make) -> Void in
+            make.centerX.equalTo()(self)
+            make.centerY.equalTo()(self)
+            make.left.equalTo()(self).with().offset()(self.MARGIN_LEFT)
+            make.right.equalTo()(self).with().offset()(-self.MARGIN_RIGHT)
+            make.top.equalTo()(self.bubbleChatImageView)
+            make.bottom.equalTo()(self.mugchatWordImageView)
+        }
+        
+        bubbleChatImageView.mas_makeConstraints { (make) -> Void in
+            make.centerX.equalTo()(self.logoView)
+            make.top.equalTo()(self.logoView)
+            make.height.equalTo()(self.bubbleChatImageView.frame.size.height)
+            make.left.equalTo()(self.logoView)
+            make.right.equalTo()(self.logoView)
+        }
+        
+        mugchatWordImageView.mas_makeConstraints { (make) -> Void in
+            make.centerX.equalTo()(self.logoView)
+            make.top.equalTo()(self.bubbleChatImageView.mas_bottom).with().offset()(self.MUGCHAT_WORD_LOGO_MARGIN_TOP)
+            make.left.equalTo()(self.logoView)
+            make.right.equalTo()(self.logoView)
+            make.bottom.equalTo()(self.logoView)
+            make.height.equalTo()(self.mugchatWordImageView.frame.height)
+        }
+
+    }
+
 }
 
 protocol SplashScreenViewDelegate {
+    
 }
+
