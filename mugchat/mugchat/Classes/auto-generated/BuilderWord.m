20e447168f4e50e33e9e765b96b4556376ad5884
Bruno <bruno.bruggemann@arctouch.com>
2014-11-28 11:29:43 -0200

    Code review suggestions

--- mugchat/mugchat/Classes/auto-generated/BuilderWord.m
+++ mugchat/mugchat/Classes/auto-generated/BuilderWord.m
@@ -1,9 +1,13 @@
 //
-//  BuilderWord.m
-//  mugchat
+// Copyright 2014 ArcTouch, Inc.
+// All rights reserved.
 //
-//  Created by Bruno Bruggemann on 11/27/14.
-//
+// This file, its contents, concepts, methods, behavior, and operation
+// (collectively the "Software") are protected by trade secret, patent,
+// and copyright laws. The use of the Software is governed by a license
+// agreement. Disclosure of the Software to third parties, in any form,
+// in whole or in part, is expressly prohibited except as authorized by
+// the license agreement.
 //
 
 #import "BuilderWord.h"
