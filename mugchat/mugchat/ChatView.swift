d1683e4be009efa4c4e986664b29709865a810b4
Ecil Teodoro <ecil.teodoro@arctouch.com>
2014-12-10 10:12:58 -0800

    Story 8423 - typing whitespaces only enabled Next button and caused app to crash

--- mugchat/mugchat/ChatView.swift
+++ mugchat/mugchat/ChatView.swift
@@ -458,7 +458,7 @@ class ChatView: UIView, UITableViewDelegate, UITableViewDataSource, UIScrollView
     // MARK: - Private methods
     
     private func updateNextButtonState() {
-        nextButton.enabled = !replyTextField.text.isEmpty
+        nextButton.enabled = !replyTextField.text.removeWhiteSpaces().isEmpty
     }
 }
 
