50725a4b0654fcddbd8f10e961ba08ac3dd90105
Ecil Teodoro <ecil.teodoro@arctouch.com>
2015-02-10 15:44:34 -0800

    Initial commit

--- flips/flips/Classes/UserFormView.swift
+++ flips/flips/Classes/UserFormView.swift
@@ -30,6 +30,7 @@ class UserFormView : UIView, UITextFieldDelegate {
     var delegate: UserFormViewDelegate?
     
     private var firstNameTextField, lastNameTextField, emailTextField, passwordTextField, birthdayTextField : UITextField!
+	private var birthdayDatePicker : UIDatePicker!
     private var isPaddingAdjusted: Bool = false
     
     //MARK: - Initialization Methods
@@ -71,8 +72,22 @@ class UserFormView : UIView, UITextFieldDelegate {
         birthdayTextField.keyboardType = UIKeyboardType.NumberPad
         birthdayTextField.returnKeyType = UIReturnKeyType.Done
         birthdayTextField.tintColor = UIColor.clearColor()
+		
+		birthdayDatePicker = UIDatePicker()
+		birthdayDatePicker.datePickerMode = UIDatePickerMode.Date
+		birthdayDatePicker.maximumDate = NSDate()
+		birthdayDatePicker.addTarget(self, action: "birthdaySelected:", forControlEvents: UIControlEvents.ValueChanged)
+		birthdayTextField.inputView = birthdayDatePicker
+		
         self.addSubview(birthdayTextField)
     }
+	
+	func birthdaySelected(sender : AnyObject?) {
+		let picker = birthdayTextField.inputView as UIDatePicker
+		var dateFormat = NSDateFormatter()
+		dateFormat.setLocalizedDateFormatFromTemplate("MM/dd/yyyy")
+		birthdayTextField.text = dateFormat.stringFromDate(picker.date)
+	}
     
     private func setupCell(placeHolder: String, leftImage: UIImage? = nil) -> UITextField {
         var textField = UITextField()
