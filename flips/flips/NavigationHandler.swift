faac7a225e2daa5b7e7c9169427b2af93020cadb
Ecil Teodoro <ecil.teodoro@arctouch.com>
2015-03-02 14:56:07 -0800

    Do not try to popToRoot when VC is an AlertVC

--- flips/flips/NavigationHandler.swift
+++ flips/flips/NavigationHandler.swift
@@ -28,18 +28,21 @@ public class NavigationHandler : NSObject {
     func onPopToRootViewControllerNotificationReceived(notification: NSNotification) {
         if let keyWindow = UIApplication.sharedApplication().keyWindow {
             if let rootViewController = keyWindow.rootViewController {
-                var rootNavigationViewController: UINavigationController = rootViewController as UINavigationController
-
-                AuthenticationHelper.sharedInstance.logout()
-                if (rootNavigationViewController.visibleViewController.navigationController != rootNavigationViewController) {
-                    rootNavigationViewController.popToRootViewControllerAnimated(false)
-                    rootNavigationViewController.visibleViewController.dismissViewControllerAnimated(true, completion: nil)
-                } else {
-                    rootNavigationViewController.popToRootViewControllerAnimated(true)
-                }
-                dispatch_async(dispatch_get_main_queue()) { () -> Void in
-                    let alertView = UIAlertView(title: NSLocalizedString("Session Expired"), message: NSLocalizedString("Please try to log in again. If the issue persists, please contact support."), delegate: nil, cancelButtonTitle: LocalizedString.OK)
-                    alertView.show()
+                println(">*>*>*>*>* TYPE: \(rootViewController.dynamicType.description())")
+                if (rootViewController.dynamicType.description() == "UINavigationController") {
+                    var rootNavigationViewController: UINavigationController = rootViewController as UINavigationController
+                    
+                    AuthenticationHelper.sharedInstance.logout()
+                    if (rootNavigationViewController.visibleViewController.navigationController != rootNavigationViewController) {
+                        rootNavigationViewController.popToRootViewControllerAnimated(false)
+                        rootNavigationViewController.visibleViewController.dismissViewControllerAnimated(true, completion: nil)
+                    } else {
+                        rootNavigationViewController.popToRootViewControllerAnimated(true)
+                    }
+                    dispatch_async(dispatch_get_main_queue()) { () -> Void in
+                        let alertView = UIAlertView(title: NSLocalizedString("Session Expired"), message: NSLocalizedString("Please try to log in again. If the issue persists, please contact support."), delegate: nil, cancelButtonTitle: LocalizedString.OK)
+                        alertView.show()
+                    }
                 }
             }
         }
