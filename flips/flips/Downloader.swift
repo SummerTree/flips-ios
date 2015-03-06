807471bcd61843135f0580a9f0c5fc444d967bcb
Alex Magalhaes <alex.magalhaes@arctouch.com>
2015-03-06 14:25:57 -0300

    Handling multiple requests for the same asset in a better way. Removing unused code.

--- flips/flips/Downloader.swift
+++ flips/flips/Downloader.swift
@@ -10,8 +10,7 @@
 // the license agreement.
 //
 
-private typealias DownloadFinished = (NSError?) -> Void
-typealias DownloadFinishedCompletion = (error: NSError?) -> Void
+public typealias DownloadFinishedCallback = ((success: Bool) -> Void)
 
 let DOWNLOAD_FINISHED_NOTIFICATION_NAME: String = "download_finished_notification"
 let DOWNLOAD_FINISHED_NOTIFICATION_PARAM_FLIP_KEY: String = "download_finished_notification_param_flip_key"
@@ -21,9 +20,7 @@ let DOWNLOAD_FINISHED_NOTIFICATION_PARAM_FAIL_KEY: String = "download_finished_n
 public class Downloader : NSObject {
     
     let TIME_OUT_INTERVAL: NSTimeInterval = 60 //seconds
-    let downloadQueue =  dispatch_queue_create("download flip queue", nil)
-    private var downloadInProgressURLs: NSHashTable!
-    
+    private var downloadInProgressURLs: Dictionary<String, [DownloadFinishedCallback]>
     
     // MARK: - Singleton Implementation
     
@@ -34,50 +31,23 @@ public class Downloader : NSObject {
         return Static.instance
     }
     
-    
     // MARK: - Initalization
     
     override init() {
+        self.downloadInProgressURLs = Dictionary<String, [DownloadFinishedCallback]>()
         super.init()
-        downloadInProgressURLs = NSHashTable()
     }
     
     // MARK: - Download Public Methods
-
-    func downloadDataFromURL(url: NSURL, localURL:NSURL, completion:((success: Bool) -> Void)) {
-        let configuration = NSURLSessionConfiguration.defaultSessionConfiguration()
-        let manager = AFURLSessionManager(sessionConfiguration: configuration)
-
-        downloadInProgressURLs.addObject(url.absoluteString!)
-
-        let request = NSMutableURLRequest(URL: url)
-        request.timeoutInterval = TIME_OUT_INTERVAL
-
-        var downloadTask = manager.downloadTaskWithRequest(request, progress: nil, destination: { (targetPath, response) -> NSURL! in
-            return localURL
-        }) { (response, filePath, error) -> Void in
-            self.downloadInProgressURLs.removeObject(url.absoluteString!)
-
-            if (error != nil) {
-                println("Could not download data from URL: \(url.absoluteString!) ERROR: \(error)")
-                completion(success: false);
-            } else {
-                completion(success: true);
-            }
+    
+    func downloadTask(url: NSURL, localURL: NSURL, completion: DownloadFinishedCallback) {
+        var absoluteString = url.absoluteString!
+    
+        if (self.downloadInProgressURLs[absoluteString] == nil) {
+            self.downloadInProgressURLs[absoluteString] = [DownloadFinishedCallback]()
         }
         
-        downloadTask.resume()
-    }
-    
-    // MARK: - New Download Method, used by StorageCache
-    
-    func downloadTask(url: NSURL, localURL:NSURL, completion:((success: Bool) -> Void)) {
-        let absoluteString = url.absoluteString!
-        if self.downloadInProgressURLs.containsObject(absoluteString) {
-            return
-        }
-        
-        downloadInProgressURLs.addObject(absoluteString)
+        self.downloadInProgressURLs[absoluteString]!.append(completion)
         
         let request = NSMutableURLRequest(URL: url)
         request.timeoutInterval = TIME_OUT_INTERVAL
@@ -87,23 +57,22 @@ public class Downloader : NSObject {
         var downloadTask = manager.downloadTaskWithRequest(request, progress: nil, destination: { (targetPath, response) -> NSURL! in
             return localURL
             }) { (response, filePath, error) -> Void in
-                self.downloadInProgressURLs.removeObject(absoluteString)
-                
+                var success: Bool
                 if (error != nil) {
                     println("Could not download data from URL: \(absoluteString) ERROR: \(error)")
-                    completion(success: false);
+                    success = false
                 } else {
-                    completion(success: true);
+                    success = true
                 }
+                
+                for callback in self.downloadInProgressURLs[absoluteString]! {
+                    callback(success: success)
+                }
+                
+                self.downloadInProgressURLs[absoluteString] = nil
         }
         
         downloadTask.resume()
     }
     
-    
-    // MARK: - Validation Methods
-    
-    private func isValidURL(url: String?) -> Bool {
-        return ((url != nil) && (url != ""))
-    }
 }
