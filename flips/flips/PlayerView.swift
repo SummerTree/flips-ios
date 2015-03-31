010936d0b1f96d8496d0ae50d8ef53bc7655f3c8
arctouch-rafaelbaggio <rafael.baggio@arctouch.com>
2015-03-31 09:50:35 -0300

    UI updates on main thread

--- flips/flips/PlayerView.swift
+++ flips/flips/PlayerView.swift
@@ -731,11 +731,12 @@ class PlayerView: UIView {
         self.playerItems = [FlipPlayerItem]()
         self.words = []
         
-        self.playButtonView.alpha = 1.0
-        self.progressBarView.alpha = 0.0
         dispatch_async(dispatch_get_main_queue(), { () -> Void in
+            self.playButtonView.alpha = 1.0
+            self.progressBarView.alpha = 0.0
             self.progressBarView.progress = 0.0
         })
+
         self.flipsDownloadProgress.removeAll()
 
         if let player = self.player() {
