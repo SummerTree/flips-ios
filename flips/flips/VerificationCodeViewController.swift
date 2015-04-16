dd16667ed417b91b9368df1823488cb97185b702
Ecil Teodoro <ecil.teodoro@arctouch.com>
2015-04-16 11:49:10 -0700

    [FLIPS-57] Pop back to settings screen after changing number

--- flips/flips/VerificationCodeViewController.swift
+++ flips/flips/VerificationCodeViewController.swift
@@ -120,7 +120,6 @@ class VerificationCodeViewController: FlipsViewController, VerificationCodeViewD
                     ActivityIndicatorHelper.hideActivityIndicatorAtView(self.view)
                     
                     self.navigationController?.popToRootViewControllerAnimated(true)
-                    self.dismissViewControllerAnimated(false, completion:nil)
                     
                     NSNotificationCenter.defaultCenter().postNotificationName(RESYNC_INBOX_NOTIFICATION_NAME, object: nil, userInfo: nil)
                 })
